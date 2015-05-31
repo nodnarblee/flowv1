0a86c78a155afdf596c1b0c2a6710aab9c5bb691
Brandon <blee298@gmail.com>
2015-05-30 22:30:18 -0700

    checkpoint commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,15 +9,15 @@ class PagesController < ApplicationController
   def inside
   end
   
-def posts
-    @posts = Post.published.page(params[:page]).per(10)
-  end
-
-  def show_post
-    @post = Post.friendly.find(params[:id])
-  rescue
-    redirect_to root_path
-  end
+  # def posts
+  #   @posts = Post.published.page(params[:page]).per(10)
+  # end
+  #
+  # def show_post
+  #   @post = Post.friendly.find(params[:id])
+  # rescue
+  #   redirect_to root_path
+  # end
 
   
   def email
