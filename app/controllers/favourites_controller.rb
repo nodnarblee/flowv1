f654d94e0d9bfd6e9b5566f561942856ea02a122
Brandon <blee298@gmail.com>
2015-06-04 21:00:21 -0700

    checkpoint commit

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -10,36 +10,13 @@ class FavouritesController < ApplicationController
                                            author_link: params[:author_link],
                                            source: params[:source])
 
-
     if article.persisted?
       head :ok
     else
       render json: { errors: article.errors.full_messages }, status: 406
     end
   end
+
 end
 
 
-# CreateFavouriteService.call( current_user, params.slice(:title, :author, etc...) )
-#
-# class CreateFavouriteService
-#
-#   # returns boolean. True if new article created.
-#   def self.call( user, data_opts = {} )
-#     raise 'favourite requires some data' if data_opts.blank?
-#
-#     article = user.articles.create(title: data_opts[:title],
-#                          author: data_opts[:author],
-#                          description: data_opts[:description],
-#                          article_link: data_opts[:article_link],
-#                          author_link: data_opts[:author_link],
-#                          source: data_opts[:source])
-#
-#     article.persisted?
-#   end
-#
-#   # helper methods here...
-#
-#
-#
-# end
